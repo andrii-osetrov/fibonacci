7bdd64542bc36b1652442c1cc4fd85446fdd645e
andrii osetrov <osetrov92@ukr.net>
2017-08-27 18:22:42 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,52 @@
+let n = parseInt(promt('Enter the number'));
+
+function print(data) {
+    document.write(data + '<br>');
+    }
+function fib(n) {
+    if (n===0) {
+        return 0;
+    }
+    else if (n===1) {
+        return 1;
+    }
+    return fib(n - 2) + fib(n - 1);
+}
+
+function bine(n) {
+    let sqrt5 = Math.sqrt(5);
+    let a = (1 + sqrt5) / 2;
+    let b = (1 - sqrt5) / 2;
+    return Math.round((Math.pow(a, n) - Math.pow(b, n)) / sqrt5);
+}
+
+function fib2(n) {
+    if (n<=2) {
+        return 1;
+    }
+else {
+    let a = 1;
+    let b = 1;
+    let res = 0;
+    for (let i = 2; i < n; i++) {
+        res = a + b;
+        a = b;
+        b = res;
+    }
+
+    return res;
+}
+    }
+    
+    function arr(n) {
+        let fib3 = [1, 1];
+        for (let i = 2; i < n; i++) {
+            fib3[i] = fib3[i - 1] + fib3[i - 2];
+        }
+//   retun fib3;
+return fib3[fib3.length - 1];
+    }
+print('Recursion: ' + fib(n));
+print('Formula Bine: ' + bine(n));
+print('Сycle: ' + fib2(n));
+print('Loop + array: ' + arr(n) + '<br>');
